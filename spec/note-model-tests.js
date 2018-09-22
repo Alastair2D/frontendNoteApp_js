6a479923c2af2a889a3b2a0e679124e8cb12b755
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-22 16:04:44 +0100

    going back to look at original passing test

--- spec/note-model-tests.js
+++ spec/note-model-tests.js
@@ -1,27 +1,37 @@
-// Write a test for instantiation of a new note
-
 (function (exports) {
 
   function test_NoteInstantiation() {
     var note = new Note('Test string')
       
-      assert.isTrue(note) === 'Test string'
+      assert1.isTrue1(note === 'Test string') 
     }
 
+
+  // function test_NoteTextReturn() {
+  //   var note = new Note('Another test string');
+     
+  //   assert.isTrue(note.returnText() === 'Another test string', 'Note model: returnText() returns correct text')
+  // }
+
+
+
   test_NoteInstantiation()
+  // test_NoteTextReturn()
 
 })(this)
 
 
 
+  // (function (exports) {
+  //   function testNoteReturn() {
+  //     var note = new Note("My favorite language is Javascript", 0);
+  //     assert.isTrue(note.noteText() === "My favorite language is Javascript", "Note model: noteText() returns correct text");
+  //   };
 
-// (function (exports) {
-
-//   function testNoteInstantiation() {
-//     var note = new Note("Test note");
-//     assert.isTrue(note.textReturn() === "Test note")
-//   };
-
-//   testNoteInstantiation();
-
-// })(this);
+  //   function testNumber() {
+  //     var note = new Note("My favorite language is Javascript", 0);
+  //     assert.isTrue(note.noteId() === 0, "Note model: noteId() returns correct id");
+  //   };
+  //   testNoteReturn();
+  //   testNumber();
+  // })(this);
